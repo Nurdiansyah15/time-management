506713078e66cc4c463273abd7f8aadc80316e57
AthallahRamadani <athallahramadani21@gmail.com>
2024-09-12 16:40:46 +0700

    make repo user

---
+++ src/main/java/com/tunduh/timemanagement/entity/Role.java
@@ -0,0 +1,6 @@
+package com.tunduh.timemanagement.entity;
+
+public enum Role {
+    ADMIN,
+    USER,
+}
--- src/main/java/com/tunduh/timemanagement/entity/UserEntity.java
+++ src/main/java/com/tunduh/timemanagement/entity/UserEntity.java
@@ -4,8 +4,13 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
 
 @Entity
@@ -16,7 +21,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -30,6 +35,9 @@ public class UserEntity {
 
     @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Column(name = "profile_picture")
     private String profilePicture;
@@ -53,5 +61,35 @@ public class UserEntity {
 
     @OneToMany(mappedBy = "user")
     private Set<UserTransactionEntity> transactions;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
 
