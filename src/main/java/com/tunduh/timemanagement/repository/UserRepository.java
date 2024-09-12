4c90a68f705fb371bb0db2809886c806ca63e825
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-09-12 23:18:56 +0700

    feat: add all directory required

--- src/main/java/com/tunduh/timemanagement/repository/UserRepository.java
+++ src/main/java/com/tunduh/timemanagement/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.tunduh.timemanagement.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
 }
