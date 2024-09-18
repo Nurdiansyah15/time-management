a20dbe8b631cfa9907d7a9c9dafc27bad8a44e82
AthallahRamadani <athallahramadani21@gmail.com>
2024-09-18 13:24:37 +0700

    adjust test folder

--- src/test/java/com/tunduh/timemanagement/TimemanagementApplicationTests.java
+++ src/test/java/com/tunduh/timemanagement/TimemanagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tunduh.timemanagement.timemanagement;
+package com.tunduh.timemanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
