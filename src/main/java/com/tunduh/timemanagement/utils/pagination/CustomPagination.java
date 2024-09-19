a78d362c5e062121b72344896541f7745558762c
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-09-19 09:43:31 +0700

    refactor(pageable): improve pagination code

--- src/main/java/com/tunduh/timemanagement/utils/pagination/CustomPagination.java
+++ src/main/java/com/tunduh/timemanagement/utils/pagination/CustomPagination.java
@@ -14,22 +14,22 @@ public class CustomPagination<T> {
     private int totalPages;
 
     public CustomPagination(List<T> content, long totalElements, int pageNumber, int pageSize) {
+        this(content, totalElements, pageNumber, pageSize, calculateTotalPages(totalElements, pageSize));
+    }
+
+    public CustomPagination(Page<T> page) {
+        this(page.getContent(), page.getTotalElements(), page.getNumber(), page.getSize(), page.getTotalPages());
+    }
+
+    private CustomPagination(List<T> content, long totalElements, int pageNumber, int pageSize, int totalPages) {
         this.content = content;
         this.totalElements = totalElements;
         this.pageNumber = pageNumber;
         this.pageSize = pageSize;
-        this.totalPages = calculateTotalPages(totalElements, pageSize);
+        this.totalPages = totalPages;
     }
 
-    public CustomPagination(Page<T> page) {
-        this.content = page.getContent();
-        this.totalElements = page.getTotalElements();
-        this.pageNumber = page.getNumber();
-        this.pageSize = page.getSize();
-        this.totalPages = page.getTotalPages();
-    }
-
-    private int calculateTotalPages(long totalElements, int pageSize) {
-        return pageSize > 0 ? (int) Math.ceil((double) totalElements / (double) pageSize) : 0;
+    private static int calculateTotalPages(long totalElements, int pageSize) {
+        return (pageSize > 0) ? (int) Math.ceil((double) totalElements / pageSize) : 0;
     }
 }
