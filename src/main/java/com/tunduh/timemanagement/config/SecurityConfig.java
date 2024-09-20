0386be550fa80632b6472808d02bcae1579c4eb9
AthallahRamadani <athallahramadani21@gmail.com>
2024-09-20 18:58:13 +0700

    fix(security): allow register

--- src/main/java/com/tunduh/timemanagement/config/SecurityConfig.java
+++ src/main/java/com/tunduh/timemanagement/config/SecurityConfig.java
@@ -72,9 +72,9 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/error", "/webjars/**", "/api/auth/login", "/oauth2/**").permitAll()
+                        .requestMatchers("/", "/error", "/webjars/**", "/api/auth/login", "/api/auth/login", "/oauth2/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/index.html", "/v3/**").permitAll()
-                        .requestMatchers("/actuator/**","/actuator/loggers").permitAll()
+                        .requestMatchers("/actuator/**", "/actuator/loggers").permitAll()
                         .requestMatchers("/error-500").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().authenticated()
