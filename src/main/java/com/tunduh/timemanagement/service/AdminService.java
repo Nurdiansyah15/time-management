1e99d8370db08d8db77c715be4e595dda4a0271e
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-09-22 14:11:01 +0700

    feat(admin): add get all user on admin feature

--- src/main/java/com/tunduh/timemanagement/service/AdminService.java
+++ src/main/java/com/tunduh/timemanagement/service/AdminService.java
@@ -2,10 +2,13 @@ package com.tunduh.timemanagement.service;
 
 import com.tunduh.timemanagement.dto.response.PurchaseResponse;
 import com.tunduh.timemanagement.dto.response.ShopItemResponse;
+import com.tunduh.timemanagement.dto.response.UserResponse;
 import com.tunduh.timemanagement.utils.pagination.CustomPagination;
+import org.springframework.data.domain.Pageable;
 
 public interface AdminService {
     CustomPagination<ShopItemResponse> getAllShopItems(int page, int size, String sort, String name, Integer maxPrice);
     CustomPagination<PurchaseResponse> getAllPurchases(int page, int size, String sort);
     //    AnalyticsResponse getUserAnalytics();
+    CustomPagination<UserResponse> getAllUsers(Pageable pageable);
 }
